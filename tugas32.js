914916cb79941652e7659dd136874f1316c39524
rahmathkd09 <rahmathkd09@gmail.com>
2020-09-22 09:56:10 +0700

    Add files via upload

---
+++ tugas32.js
@@ -0,0 +1,9 @@
+function eksternal() {
+    var namadepan = document.getElementById('namadepan')
+    var namabelakang = document.getElementById('namabelakang')
+    var hobi = document.getElementById('hobi')
+    console.log(namadepan)
+    console.log(namabelakang)
+    console.log(hobi)
+}
+eksternal()

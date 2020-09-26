d449d0d4252806a996f9300d4769c9bdf9b6bc1f
rahmathkd09 <rahmathkd09@gmail.com>
2020-09-26 10:45:02 +0700

    Add files via upload

---
+++ tugas3_jumat/precache-manifest.9a9f6deb57c7efef0262d39315a6bec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c399df62f7e0c42869122930ad7b574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab03fd24a7dc7ce2ca8c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32c4af2ae81c6d331a2b",
+    "url": "/static/js/2.a4a55421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4a55421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab03fd24a7dc7ce2ca8c",
+    "url": "/static/js/main.799253ad.chunk.js"
+  },
+  {
+    "revision": "084acc2c99a3650d5848",
+    "url": "/static/js/runtime-main.423c99e8.js"
+  }
+]);

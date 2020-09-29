8255e1a3544ce2bd66be33d96c45e97fb945d8bc
rahmathkd09 <rahmathkd09@gmail.com>
2020-09-29 14:42:33 +0700

    Add files via upload

---
+++ tugas_4_reactjs_dev/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import MenuMakanan from './Page/MenuMakanan'
+
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div align="center">
+      
+      <MenuMakanan />
+      
+      
+
+      </div>
+      );
+
+
+
+
+
+        }
+
+
+  }
+
+  export default App
---
+++ tugas_4_reactjs_dev/src/Page/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header(){
+    return(
+    	<div align="center>">
+        <h3>Home | Product | Kontak | Tentang Kami</h3>
+        </div>
+    )
+}
+export default Header

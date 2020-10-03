df026086f33e940ca2a36bd76499055248808698
rahmathkd09 <rahmathkd09@gmail.com>
2020-10-03 09:18:52 +0700

    Add files via upload

---
+++ tugas_7_reactjs_prod/precache-manifest.42e1fe96120de07ee1fd80e3f94488b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef5f5d55ca2159c3a8726c37d5c2356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0658cec8eb228c231093",
+    "url": "/static/js/2.60d5856c.chunk.js"
+  },
+  {
+    "revision": "f1a51298dffc8b16b407",
+    "url": "/static/js/main.7e8d25a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
